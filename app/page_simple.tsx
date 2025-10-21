857e1d00cca1e23f680eebcdb2db80557087099b
jgero <jgero@example.com>
2025-10-21 12:39:49 +0400

    Fix-TypeScript-build-errors

--- app/page_simple.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-valorant-dark relative overflow-hidden">
-      <h1>Test Page</h1>
-    </div>
-  );
-}
